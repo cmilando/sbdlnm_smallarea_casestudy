0a9efc67ba8352a800e4fe4e7fc48c06f9131417
Chad Milando <44262671+cmilando@users.noreply.github.com>
2025-09-27 10:50:50 -0400

    init

--- 03_plot_sbdlnm.R
+++ 03_plot_sbdlnm.R
@@ -37,7 +37,7 @@ shapefile_bcn <- read_sf("input/shapefile_bcn.shp")
 # WARNING: WE LOAD THE OUTPUT FROM MODEL 3 EXECUTED IN THE PAPER WITH THE 
 # REAL MORTALITY DATA. IT CAN BE REPLACED FOR ANY OF THE OTHER OUTPUTS OBTAINED
 # WITH REAL DATA OR OBTAINED WITH THE PREDICTED DATA FROM CODE 02_run_sbdlm.R
-load("input/result_paper/final_simsmatrix_model3_spatial_casecrossover.RData")
+load("input/result_paper/final_simsmatrix_model1_independent_casecrossover.RData")
 load("output/dlnm_configuration.RData")
 
 # DATA PREPARATION
@@ -112,141 +112,6 @@ cb <- lapply(1:dlnm_var$n_reg, function(i_reg) {
   
 })
 
-Center_RR <- function(f.rr, f.cen, f.temp){
-  
-  cen <- f.temp[which.min(abs(f.temp - f.cen))]
-  rr <- apply(f.rr, 2, function(x) x - x[f.temp == cen])
-  rr
-  
-}
-
-#--------------------------------------------------------------
-# A) PANEL 3-DIMENSIONAL TEMPERATURE-LAG-RISK ASSOCIATION
-# FOR REGION 1 / ITERATION 1 (SAME AS FIRST PANEL FIGURE 1 IN THE PAPER)
-#--------------------------------------------------------------
-
-# Specify the neighborhood of Barcelona (any integer from 1 to 73)
-i_reg <- 1 
-
-basis_exp <- basis_all[[i_reg]]$basis_exp
-basis_lag <- basis_all[[i_reg]]$basis_lag
-f.x_temp <- x_temp[,i_reg]
-
-# Extract the ensemble of sample coefficients of the crossbasis
-beta_reg <- 
-  winbugs_res[, grepl(
-    paste0("^beta\\[", i_reg,","), colnames(winbugs_res))]
-
-# Initialize the array to store the RR for each temperature and lag combination
-cp <- array(NA, dim = c(nrow(beta_reg), length(f.x_temp), dlnm_var$max_lag + 1))
-
-for(i_iter in 1:nrow(beta_reg)) {
-  for(i_temp in 1:length(f.x_temp)) {
-    for(i_lag in 1:(dlnm_var$max_lag + 1)) {
-      cp[i_iter, i_temp, i_lag] <- sum(
-        (t(basis_exp[i_temp,,drop = FALSE]) %*% basis_lag[i_lag,, drop = FALSE]) *
-          matrix(beta_reg[i_iter,], ncol = ncol(basis_lag), byrow = TRUE))
-    }
-  }
-}
-rm(i_iter, i_temp, i_lag)
-
-# Position of the temperature percentile where we center the associations
-i_cen <- percentiles == 0.3
-
-# New array to store the centered association
-cp_cen <- array(NA, dim = c(nrow(beta_reg), length(f.x_temp), dlnm_var$max_lag + 1))
-
-for(i in 1:length(f.x_temp)){
-  cp_cen[,i,] <- cp[,i,]-cp[,i_cen,]
-}; rm(i, cp)
-
-# Choose an iteration for plotting (e.g., the first iteration)
-cp_plot <- exp(cp_cen[1,,])
-
-# Plot three-dimensional associations
-pdf("plot/figureA_exposure_lag_response_surface.pdf", 
-    width = 5, height = 5)
-par(mex = 0.3)
-association <- 
-  persp(x = f.x_temp, y = 0:dlnm_var$max_lag, z = cp_plot, border = NA,
-        ticktype = "detailed", theta = 240, phi = 50,
-        xlab = "\nTemperature (ºC)", ylab = "\nLag (days)", zlab = "\nRelative risk", 
-        col = "lightskyblue", zlim = c(min(cp_plot), max(cp_plot)), 
-        ltheta = 170, shade = 0.75, r = sqrt(3), d = 5, 
-        cex.lab = 0.8, cex.axis = 0.8, main = "A) Exposure-lag-response surface")
-
-# Add lines for risk at lag 1, risk at percentile 99, and centring temperature
-lines(trans3d(x = f.x_temp, y = 1,
-              z = cp_plot[,2],
-              pmat = association), lwd = 2, col = "red")
-lines(trans3d(x = f.x_temp[percentiles == 0.99], y = 0:dlnm_var$max_lag,
-              z = cp_plot[percentiles == 0.99,],
-              pmat = association), lwd = 2, col = "orange")
-lines(trans3d(x = f.x_temp[i_cen], y = 0:dlnm_var$max_lag,
-              z = cp_plot[i_cen,],
-              pmat = association), lwd = 2, col = "black")
-
-dev.off()
-
-# Clean up variables
-rm(i_reg, basis_exp, basis_lag, f.x_temp, beta_reg, cp_cen, i_cen, association, 
-   cp_plot)
-
-#--------------------------------------------------------------
-### B) OVERALL CUMULATIVE TEMPERATURE-MORTALITY ASSOCIATION FOR REGION 1
-### (SAME AS FIGURE 2 IN THE PAPER)
-#--------------------------------------------------------------
-
-# Specify the neighborhood of Barcelona (any integer from 1 to 73)
-i_reg <- 1 
-
-# Extract the ensemble of sample coefficients of the crossbasis
-beta_reg <- 
-  winbugs_res[, grepl(
-    paste0("^beta\\[", i_reg,","), colnames(winbugs_res))]
-
-# Calculate directly the RR of the overall cumulative temperature-mortality 
-# associations for region i_reg
-rr <- apply(beta_reg, 1, function(x) {
-  sapply(1:length(x_temp[,i_reg]), function(i) cb[[i_reg]][i,] %*% x)
-})
-
-# Keep the exposure variable for the region
-x_plot <- x_temp[, 1]
-
-# Center the curve
-i_cen <- percentiles == 0.3
-rr_plot <- t(apply(rr, 1, function(x) x - rr[i_cen,]))
-
-# Plot the overall cumulative temperature-mortality association
-pdf("plot/figureB_overall_cumulative-temperature_mortality_association.pdf", 
-    width = 6, height = 5)
-plot(x_plot, rep(1, length(x_plot)), type = "n", log = "y",
-     xlim = range(x_plot), ylim = c(0.5, 5),
-     xlab = "Temperature (ºC)", ylab = "Relative risk",
-     main = "B) Overall cumulative temperature-mortality association",
-     axes = FALSE)
-axis(1, at = seq(15, 40, by = 5))
-axis(2, at = c(0.5, seq(1, 5, by = 1)), labels = c(0.5, seq(1, 5, by = 1)))
-
-# Draw the ensemble of overall accumulated associations
-for(i in 1:ncol(rr_plot)){
-  lines(x_plot, exp(rr_plot[,i]), lwd = 0.2, col = "lightgrey")
-}; rm(i)
-abline(h = 1)
-
-# Calculate and draw the summary associations
-rr_plot_summary <- apply(rr_plot, 1, quantile, c(0.025, 0.5, 0.975))
-lines(x_plot, exp(rr_plot_summary[2,]), lwd = 3)
-lines(x_plot, exp(rr_plot_summary[1,]), lwd = 2, lty = 2)
-lines(x_plot, exp(rr_plot_summary[3,]), lwd = 2, lty = 2)
-
-dev.off()
-
-# Clean up variables
-rm(i_reg, beta_reg, rr, x_plot, i_cen, rr_plot, rr_plot_summary)
-
 #--------------------------------------------------------------
 ### C) MAP OF THE RISKS AT PERCENTILE 99
 ### (SAME AS FIGURE 4C IN THE PAPER)
@@ -304,14 +169,38 @@ pal <- colorNumeric(palette = rev(
     "#2E5A87")), domain = c(log(rr_min), log(rr_max)), reverse = FALSE)
 
 # Plot the map with the risks
-pdf("plot/figureC_map_relative_risks.pdf", 
-    width = 3, height = 5)
-
-par(mar = c(1, 1, 1, 1), omi = c(0, 0, 0.4, 0))
+# pdf("plot/figureC_map_relative_risks.pdf", 
+#     width = 3, height = 5)
+# 
+# par(mar = c(1, 1, 1, 1), omi = c(0, 0, 0.4, 0))
 plot(shapefile_bcn$geometry, col = pal(log(rr_plot)), 
      main = "C) Map relative risks")
 
-dev.off()
+#' ============================================================================
+shapefile_bcn$rr_plot <- rr_plot
+shapefile_bcn$i_reg <- 1:73
+bcn_coords <- st_centroid(shapefile_bcn)
 
-# Clean up variables
-rm(rr, Center_RR, rr_plot, rr_max, rr_min, pal)
+library(sf)
+library(tidyverse)
+library(patchwork)
+library(ggpubr)
+
+p1 <- ggplot(shapefile_bcn) + theme_void() +
+  geom_sf(aes(fill = rr_plot)) + 
+  scale_fill_gradient2(low = '#2E5A87', mid = 'white',midpoint = 1,
+                       high = '#A90C38', name= 'RR') +
+  geom_sf_text(data = bcn_coords, aes(label = i_reg), size = 2)
+
+p2 <- ggplot(shapefile_bcn) + theme_void() +
+  geom_sf(aes(fill = ifelse(rr_plot > 2, 2, rr_plot))) +
+  scale_fill_gradient2(low = '#2E5A87', mid = 'white',midpoint = 1,
+                       high = '#A90C38', name = 'lim(RR, 2)') +
+  geom_sf_text(data = bcn_coords, aes(label = i_reg), size = 2)
+
+p1 + ggtitle("a. RR as is") +
+  plot_spacer() +
+  p2 + ggtitle("b. RR limited to max of 2") + 
+  plot_layout(ncol = 3, widths = c(0.45, 0.1, 0.45))
+
+ggsave("fig1.png", width = 8, height = 5, dpi = 400)
---
+++ 04_check_individual.R
@@ -0,0 +1,158 @@
+################################################################################
+# IMPORTANT: THE MORTALITY DATA provided are simulated FROM MODEL 4 IN THE
+# PAPER (SB-DLNM WITH TIME-SERIES DESIGN) due to confidentiality restrictions of 
+# the original data set. Original data may be requested to the data owner (The 
+# Barcelona Public Health Agency) who should share them in similar terms than 
+# those applying to this study. The supplied mortality, with exactly the same 
+# structure than the original data set, allows reproducing the code provided so 
+# that interested readers may run it as an example of use. However, WinBUGS 
+# results from our original analyses are additionally supplied (input/result_paper
+# folder) so that readers should be able to reproduce exactly our analyses after 
+# those WinBUGS calls (model summaries and plots).
+################################################################################
+
+################################################################################
+# In this R project we implement Bayesian and Spatial Bayesian Distributed 
+# Lag Non-Linear Models (B-DLNM and SB-DLNM) for the case Study of short-term 
+# associations between temperature and mortality in the city of Barcelona.
+################################################################################
+
+################################################################################
+# CODE 1: DATA PREPARATION
+# Data preparation: Transforming time-series temperature and mortality data sets
+# for B-DLNMs and SB-DLNMs.
+################################################################################
+
+# Load libraries
+
+library(sf)
+library(spdep)
+library(lubridate)
+library(dlnm)
+library(gnm)
+library(splines)
+library(rstan)
+library(cmdstanr)
+library(tidyverse)
+library(foreign)
+library(shinystan)
+# Load data
+
+load("input/daily_data.RData")
+
+# Set variables defining the dlnm model
+load("output/dlnm_configuration.RData")
+
+# Set variables for trend and seasonality
+
+# Subset data to only summer months of 2007 to 2016
+data <- subset(data, month(date) %in% 6:9)
+data <- subset(data, year(date) %in% 2007:2016)
+
+data$year <- factor(lubridate::year(data$date))
+
+# Create crossbasis for each region
+
+# Ensure that the data is ordered by region to maintain alignment between 
+# crossbasis and regions
+
+if(is.unsorted(data$region)) {
+  stop("data needs to be ordered by region for the next loop")}
+
+list_X <- vector("list", dlnm_var$n_reg)
+
+for(i_reg in 1:dlnm_var$n_reg) {
+  
+      ## this is all the same as in 01_data_preparation.R
+  
+      temp <- subset(data, region == i_reg, 
+                     select = c("temp", paste0("lag", 1:dlnm_var$max_lag)))
+      
+      temp_knots <- quantile(temp$temp, dlnm_var$var_prc, na.rm = TRUE)
+      temp_boundary <- range(temp, na.rm = TRUE)
+      
+      cb <- crossbasis(temp,
+                       argvar = list(fun = dlnm_var$var_fun,
+                                     knots = temp_knots,
+                                     Boundary.knots = temp_boundary),
+                       arglag = list(fun = "ns",
+                                     knots = logknots(dlnm_var$max_lag, 
+                                                      dlnm_var$lagnk),
+                                     intercept = TRUE))
+      
+      cb
+      
+  
+  ##
+  list_X[[i_reg]] <- cb
+}
+
+dim(cb)        
+tail(cb)
+
+
+# PREPARE DATA FOR THE CASE-CROSSOVER DESIGN
+
+# Create strata for the case-crossover
+# (neighborhood - year - month - day of week)
+data$strata <- paste(data$region, 
+                     year(data$date), 
+                     formatC(month(data$date), width = 2, flag = "0"),
+                     wday(data$date, week_start = 1),
+                     sep = ":")
+
+# make them both lists
+data_l <- split(data, f = data$region)
+
+#' ===========================================================================
+#' Normal Cond and Poisson
+
+library(gnm)
+
+# just try 1 area that seems like it has a strong relationship
+# region 39
+df_local <- data_l[[39]]
+cb_local <- list_X[[39]]
+
+# keep just the non-empty strata
+df_local_agg <- df_local %>%
+  group_by(strata) %>%
+  summarize(
+    .groups = 'keep',
+    sum_mort = sum(mort)
+  ) %>% 
+  mutate(keep = 1)
+
+df_local <- left_join(df_local, df_local_agg)
+
+# quasi-poisson
+m1.std <- gnm(mort ~ cb_local + factor(strata), data = df_local,
+              family = quasipoisson,
+              subset = keep == 1)
+cp1 <- crosspred(cb_local, m1.std)
+plot(cp1, 'overall')
+
+# but are the same as the base model
+load("input/result_paper/final_simsmatrix_model1_independent_casecrossover.RData")
+for(i_reg in 1:73) {
+  beta_reg1 <- winbugs_res[,grepl(paste0("^beta\\[", i_reg,","), 
+                                  colnames(winbugs_res))]
+  xx <- cbind("paper" = apply(beta_reg1, 2, median), 
+        "freq-P" = coef(m1.std)[2:13] )
+  if(sum(apply(xx, 1, function(a) abs(a[1] - a[2])^2)) < 1 ) stop()
+  print(xx)
+}
+rm(winbugs_res); gc();
+
+load("input/result_paper/final_simsmatrix_model2_independent_timseseries.RData")
+for(i_reg in 1:73) {
+  beta_reg1 <- winbugs_res[,grepl(paste0("^beta\\[", i_reg,","), 
+                                  colnames(winbugs_res))]
+  xx <- cbind("paper" = apply(beta_reg1, 2, median), 
+              "freq-P" = coef(m1.std)[2:13] )
+  if(sum(apply(xx, 1, function(a) abs(a[1] - a[2])^2)) < 1 ) stop()
+  print(xx)
+}
+rm(winbugs_res); gc();
+
+
